9b1dc976bf46ff0abb4cb3d7dc46638efe1868f0
prasathnarayanan6 <prasathnarayanan6@gmail.com>
2025-07-09 17:57:05 +0530

    New changes

--- client/src/components/trainee/TraineeDashboard.js
+++ client/src/components/trainee/TraineeDashboard.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import SideBar from '../sideBar';
 import NavBar from '../navBar';
+import { LayoutDashboard } from 'lucide-react';
 
 function TraineeDashboard() {
   const [buttonOpen, setButtonOpen] = useState(true);
@@ -20,6 +21,22 @@ function TraineeDashboard() {
         } `}>
                         <div>
                                 <NavBar />
+                        </div>
+                        <div className="bg-gray-100 h-screen">
+                                <div className="px-2 grid grid-cols-2 gap-4">
+                                        <div className="border bg-white rounded-sm mt-3">
+                                              <div className="m-2 flex gap-4 items-center">
+                                                <div className="text-white bg-[#8DC63F] p-2 rounded-full"><LayoutDashboard size={21}/></div>
+                                                <span className="text-lg text-gray-500">General Dashboard</span>
+                                              </div>
+                                              <div className="">
+
+                                              </div>
+                                        </div>
+                                        <div className="border bg-white rounded-sm mt-3">
+                                              <div></div>
+                                        </div>
+                                </div>
                         </div>
                 </div>
     </div>
