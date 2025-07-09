14d4f11b51c62cbd25613b4b86cbc0fbcd3beb5d
prasathnarayanan6 <prasathnarayanan6@gmail.com>
2025-07-09 18:15:59 +0530

    frontend dashboard design started

--- client/src/components/trainee/TraineeDashboard.js
+++ client/src/components/trainee/TraineeDashboard.js
@@ -23,18 +23,22 @@ function TraineeDashboard() {
                                 <NavBar />
                         </div>
                         <div className="bg-gray-100 h-screen">
-                                <div className="px-2 grid grid-cols-2 gap-4">
-                                        <div className="border bg-white rounded-sm mt-3">
+                                <div className="px-2 grid grid-cols-3 gap-4">
+                                        <div className="border bg-white rounded-sm mt-3 col-span-2">
                                               <div className="m-2 flex gap-4 items-center">
                                                 <div className="text-white bg-[#8DC63F] p-2 rounded-full"><LayoutDashboard size={21}/></div>
                                                 <span className="text-lg text-gray-500">General Dashboard</span>
                                               </div>
-                                              <div className="">
-
+                                              <div className="border border-t-1 border-r-0 border-l-0 border-b-0 p-4">
+                                                    <div className="grid grid-cols-3 gap-4">
+                                                            <div className="border p-2 bg-blue-100">sodfj</div>
+                                                            <div className="border p-2 bg-blue-100">dh</div>
+                                                            <div className="border p-2 bg-blue-100">skfj</div>
+                                                    </div>
                                               </div>
                                         </div>
-                                        <div className="border bg-white rounded-sm mt-3">
-                                              <div></div>
+                                        <div className="col-span-1 border bg-white rounded-sm mt-3">
+                                              <div>dcf</div>
                                         </div>
                                 </div>
                         </div>
