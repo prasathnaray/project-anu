6b258854f56c6e5ad4db2b826f25019b19f3e71e
Prasath Narayanan <prasathnarayanan6@gmail.com>
2025-07-17 22:52:28 +0530

    Committed some changes

--- client/src/components/admin/CreateBatch.js
+++ client/src/components/admin/CreateBatch.js
@@ -1,10 +1,31 @@
-import React from "react";
-
-function CreateBatch() {
+import React, {useState} from "react"; 
+import { twMerge } from "tailwind-merge";
+function CreateBatch({isVisible,  onClose, children}){
+    const [shake, setShake] = useState(false);
+    if (!isVisible) return null;
+    // const handleClose = (e) => {
+    //     if(e.target.id === 'wrapper') onClose();
+    // }
+    const handleWrapperClick = () => {
+                setShake(true);
+                setTimeout(() => setShake(false), 500); // Reset after animation duration
+    };
     return (
-        <div>
-            
-        </div>
-    )
+        <div
+      className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-xs flex justify-center items-center z-50"
+      id="wrapper"
+      onClick={handleWrapperClick}
+    >
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className={twMerge(
+          "w-[700px] transition-all",
+          shake ? "animate-shake" : ""
+        )}
+      >
+        <div className="bg-white p-4 rounded shadow-md">{children}</div>
+      </div>
+    </div>
+    ) 
 }
 export default CreateBatch;
--- client/src/pages/Batch.js
+++ client/src/pages/Batch.js
@@ -2,12 +2,52 @@ import React, {useState} from "react";
 import SideBar from "../components/sideBar";
 import NavBar from "../components/navBar";
 import { ArrowUpWideNarrow, ChevronLeft, ChevronRight } from "lucide-react";
+import CreateBatch from "../components/admin/CreateBatch";
+import { jwtDecode } from "jwt-decode";
+import { Navigate } from "react-router-dom";
+import { X } from 'lucide-react';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+const CustomDateInput = React.forwardRef(({ value, onClick, onChange }, ref) => (
+  <div className="relative w-full mt-5">
+    <input
+      ref={ref}
+      onClick={onClick}
+      value={value}
+      readOnly
+      placeholder=" "
+      className="peer w-full px-2.5 pt-3 pb-2.5 text-sm text-gray-900 border border-gray-300 rounded-sm bg-white appearance-none focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+    />
+    <label
+      className="absolute text-sm text-gray-600 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 
+                                        peer-focus:px-2 
+                                        peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2
+                                        peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4
+                                                       peer-focus:text-blue-600
+
+                                        rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
+    >
+      End Date
+    </label>
+  </div>
+));
 function Batch()  {
-    const [buttonOpen, setButtonOpen] = useState(true);
-    const handleButtonOpen = () => {
-        setButtonOpen(!buttonOpen);
-    };
-    const token = localStorage.getItem('user_token')
+        const [openBatch, setOpenBatch] = useState(false);
+        const [startDate, setStartDate] = useState(null);
+        const [endDate, setEndDate] = useState(null);
+        const handleClose = () => setOpenBatch(false);
+        const [buttonOpen, setButtonOpen] = useState(true);
+        const handleButtonOpen = () => {
+                setButtonOpen(!buttonOpen);
+        };
+        const token = localStorage.getItem('user_token')
+        if (!token) {
+                        return <Navigate to="/" replace />;
+        }
+        const decoded = jwtDecode(token);
+        if (decoded.role != 101 && decoded.role != 102) {
+                        return <Navigate to="/" replace />;
+        }
     return (
         <div className={`flex`}>
                 <div>
@@ -22,7 +62,7 @@ function Batch()  {
                                 <NavBar />
                        </div>
                         <div className="bg-gray-100 h-screen">
-                                <div className="px-10 py-8 w-full max-w-[1800px] mx-auto">
+                                <div className="px-[130px] py-8 w-full max-w-[1800px] mx-auto">
                                             <div className="text-gray-500">Dashboard / Batch</div>
                                             <div className="mt-5 font-semibold text-xl text-gray-600">Batches</div>
                                             <div className="mt-5 bg-white rounded px-8 py-10 ">
@@ -34,11 +74,11 @@ function Batch()  {
                                                             name="reset_password_mail"
                                                             className="rounded px-2 py-2 w-full mb-6 focus:outline-none focus:ring-0 border mt-4"
                                                         /></div>
-                                                        <div className="flex justify-end items-center"><a href="/trainee/add" className="bg-[#8DC63F] hover:bg-[#8DC63F] text-white rounded px-10 py-3 font-semibold text-sm transition-all ease-in-out">Create Batch</a></div>
+                                                        <div className="flex justify-end items-center"><button className="bg-[#8DC63F] hover:bg-[#8DC63F] text-white rounded px-10 py-3 font-semibold text-sm transition-all ease-in-out" onClick={() => setOpenBatch(true)}>Create Batch</button></div>
                                                 </div>
                                                 <table className="w-full text-left border-collapse">
                                                         <thead>
-                                                                <tr className="border-b border-gray-300 shadow-sm">
+                                                                <tr className="border-b border-gray-300 shadow-sm text-sm">
                                                                         <th className="py-2 px-4"></th>
                                                                         <th className="py-2 px-4 text-[#8DC63F] flex items-center gap-2"><div>Batch Name </div><button className=""><ArrowUpWideNarrow size={20}/></button></th>
                                                                         <th className="py-2 px-4 text-[#8DC63F]"><div className="flex items-center gap-2"><span>Start date</span><button className=""><ArrowUpWideNarrow size={20} /></button></div></th>
@@ -64,6 +104,68 @@ function Batch()  {
                                 </div>
                         </div>
                 </div>
+                <CreateBatch isVisible={openBatch} onClose={handleClose}>
+                    <div className="flex justify-between items-center">
+                        <div className="text-lg">Create Batch</div>
+                        <div><button onClick={handleClose} className="text-red-500 hover:bg-red-50 p-1 hover:rounded"><X size={24}/></button></div>
+                    </div>
+                    <div className="relative mt-5">
+                        <input
+                        type="text"
+                        id="Name"
+                        className="block px-2.5 pb-2.5 pt-3 w-full text-sm text-gray-900 bg-transparent rounded-sm border border-gray-300 appearance-none focus:outline-none focus:ring-1 focus:ring-blue-600 focus:border-blue-600 peer"
+                        placeholder=" "
+                        name="trainee_name"
+                        />
+                        <label
+                        htmlFor="Name"
+                        className="absolute text-sm text-gray-600 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 
+                                        peer-focus:px-2 
+                                        peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2
+                                        peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4
+                                                       peer-focus:text-blue-600
+
+                                        rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
+                        >
+                        Batch Name
+                        </label>
+                    </div>
+                    <div className="grid grid-cols-2 gap-5">
+                        <div className="relative mt-5">
+                                <input
+                                type="text"
+                                id="Name"
+                                className="block px-2.5 pb-2.5 pt-3 w-full text-sm text-gray-900 bg-transparent rounded-sm border border-gray-300 appearance-none focus:outline-none focus:ring-1 focus:ring-blue-600 focus:border-blue-600 peer"
+                                placeholder=" "
+                                name="trainee_name"
+                                />
+                                <label
+                                htmlFor="Name"
+                                className="absolute text-sm text-gray-600 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 
+                                                peer-focus:px-2 
+                                                peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2
+                                                peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4
+                                                peer-focus:text-blue-600
+                                                rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
+                                >
+                                Start Date
+                                </label>
+                        </div>
+                        <div className="relative">
+                                        <DatePicker
+                                                id="endDate"
+                                                selected={endDate}
+                                                onChange={(date) => setEndDate(date)}
+                                                dateFormat="yyyy-MM-dd"
+                                                customInput={<CustomDateInput />}
+                                                // popperPlacement="bottom-start"      
+                                        />
+                        </div>
+                    </div>
+                    <div className="mt-5 flex justify-end items-center">
+                        <button className="bg-[#8DC63F] px-3 py-2 rounded-sm text-white">Save</button>
+                    </div>
+                </CreateBatch>
     </div>
     )
 }
