16d79acdbe73934588b715419b75525a845516f9
Prasath Narayanan <prasathnarayanan6@gmail.com>
2025-07-25 16:16:43 +0530

    Created api dashboard fetch api

--- client/src/components/admin/AdminDashboard.js
+++ client/src/components/admin/AdminDashboard.js
@@ -1,13 +1,32 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import { jwtDecode } from "jwt-decode";
 import NavBar from "../navBar";
 import SideBar from "../sideBar";
-import { LayoutDashboard } from "lucide-react";
+import { ClipboardPenLine, GraduationCap, LayoutDashboard, NotepadText } from "lucide-react";
+import getDashboardAPI from "../../API/dashboardAPI";
 function AdminDashboard(){
     const [buttonOpen, setButtonOpen] = useState(true);
     const handleButtonOpen = () => {
         setButtonOpen(!buttonOpen);
     };
+    const [dashboardData, setDashboardData] = useState([])
+    const handleDashboardApi = async() => {
+        try
+        {   
+            const token = localStorage.getItem('user_token');
+            const result = await getDashboardAPI(token);
+            setDashboardData(result.data);
+        }
+        catch(err)
+        {
+            console.log(err)
+        }
+    }
+    useEffect(() => {
+        handleDashboardApi()
+        //console.log(dashboardData);
+    }, [])
+    console.log(dashboardData)
     return (
         <div className={`flex`}>
                 <div>
@@ -30,9 +49,27 @@ function AdminDashboard(){
                                                             </div>
                                                             <div className="border border-t-1 border-r-0 border-l-0 border-b-0 p-4">
                                                                         <div className="grid grid-cols-3 gap-4">
-                                                                                <div className="border p-2 bg-blue-100">sodfj</div>
-                                                                                <div className="border p-2 bg-blue-100">dh</div>
-                                                                                <div className="border p-2 bg-blue-100">skfj</div>
+                                                                                <div className="border p-2 rounded">
+                                                                                       <span className="font-semibold px-2">Trainees (in total)</span>
+                                                                                       <div className="flex justify-between items-center px-2 pt-5">
+                                                                                                    <div className="text-[#8DC63F]"><GraduationCap size={43}/></div>
+                                                                                                    <div className="text-3xl text-gray-600">{dashboardData[1]?.count || 0}</div>
+                                                                                       </div>
+                                                                                </div>
+                                                                                <div className="border p-2 ">
+                                                                                        <span className="font-semibold">Instrutors (in total)</span>
+                                                                                         <div className="flex justify-between items-center px-2 pt-5">
+                                                                                                    <div className="text-[#8DC63F]"><ClipboardPenLine size={43}/></div>
+                                                                                                    <div className="text-3xl text-gray-600">{dashboardData[0]?.count || 0}</div>
+                                                                                         </div>
+                                                                                </div>
+                                                                                <div className="border p-2">
+                                                                                        <span className="font-semibold">Batches (in total)</span>
+                                                                                         <div className="flex justify-between items-center px-2 pt-5">
+                                                                                                    <div className="text-[#8DC63F]"><GraduationCap size={43}/></div>
+                                                                                                    <div className="text-3xl text-gray-600">80</div>
+                                                                                         </div>
+                                                                                </div>
                                                                         </div>
                                                             </div>
                                                 </div>
