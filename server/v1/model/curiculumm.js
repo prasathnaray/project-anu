b9fb4eb11ed02aa6e8913bf0ae6e4a0f96a1f0bc
Prasath Narayanan <prasathnarayanan6@gmail.com>
2025-08-04 18:27:04 +0530

    Changes

--- server/v1/model/curiculumm.js
+++ server/v1/model/curiculumm.js
@@ -1,15 +1,15 @@
 const client = require('../utils/conn');
 const curiculumm = (curiculum_name, requester) => {
     const isPrivileged = [101,  102].includes(Number(requester.role));
-    if(!isPrivileged)
-    {
-        return resolve({
-            status: 'Unauthorized',
-            code: 401,
-            message: 'You do not have permission to view trainee profiles'
-        })
-    }
     return new Promise((resolve, reject) => {  
+        if(!isPrivileged)
+        {
+            return resolve({
+                status: 'Unauthorized',
+                code: 401,
+                message: 'You do not have permission to view trainee profiles'
+            })
+        }
         client.query('INSERT INTO public.curiculum_data(curiculum_nam) VALUES($1)', [curiculum_name] ,(err, result) => {
             if(err)
             {
