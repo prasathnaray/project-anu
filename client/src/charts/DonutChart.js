3bfa34974dd70f3c612ca89e3d82aeb93de8c506
Prasath Narayanan <prasathnarayanan6@gmail.com>
2025-09-12 16:57:35 +0530

    error but will fix it

--- client/src/charts/DonutChart.js
+++ client/src/charts/DonutChart.js
@@ -1,46 +1,49 @@
 import * as React from 'react';
 import { PieChart, pieArcClasses, pieClasses } from '@mui/x-charts/PieChart';
-import axios from 'axios';
 import { useState } from 'react';
 import GetgenderRatioAPI from '../API/GetgenderRatioAPI';
 
-export default function BasicPie() {
+export default function CompletionStats() {
   const palette = ['#8DC63F', '#36A2EB', '#FFC107', '#FF5722'];
   const [genderAnalytics, setGenderAnalytics] = useState([]);
 
   const handleAnalytics = async () => {
     try {
       const token = localStorage.getItem('user_token');
-      const response = await GetgenderRatioAPI(token)
+      const response = await GetgenderRatioAPI(token);
       const transformed = response.data.map((item, idx) => ({
         id: idx,
         value: Number(item.count),
         label: item.user_gender
       }));
-
       setGenderAnalytics(transformed);
     } catch (err) {
       console.log(err);
     }
   };
+
   React.useEffect(() => {
     handleAnalytics();
   }, []);
+
   return (
     <PieChart
-      colors={palette}
-      sx={{
-        [`.${pieClasses.series}[data-series="outer"] .${pieArcClasses.root}`]: {
-          opacity: 0.6,
-        },
-      }}
       series={[
         {
           data: genderAnalytics,
+          innerRadius: 15,
+          outerRadius: 25,
+          cornerRadius: 3,
         },
       ]}
-      width={200}
-      height={200}
+      colors={palette}
+      width={70}
+      height={70}
+      sx={{
+        [`.${pieClasses.series}[data-series="outer"] .${pieArcClasses.root}`]: {
+          opacity: 0.8,
+        },
+      }}
     />
   );
 }
--- client/src/pages/CourseInd.js
+++ client/src/pages/CourseInd.js
@@ -3,11 +3,19 @@ import { jwtDecode } from 'jwt-decode'
 import { Navigate } from 'react-router-dom';
 import NavBar from '../components/navBar';
 import SideBar from '../components/sideBar';
+import { useParams } from 'react-router-dom';
+import { IconButton } from '@mui/material';
+import { Plus } from 'lucide-react';
+import CollapsibleTable from '../components/CourseTable';
+import EnhancedTable from '../components/CourseTable';
 function CourseInd() {
   const [buttonOpen, setButtonOpen] = React.useState(true);
   const handleButtonOpen = () => {
     setButtonOpen(!buttonOpen);
   };
+
+  const url = useParams();
+  console.log(url)
   let token = localStorage.getItem('user_token');
   const decoded = jwtDecode(token);
   if (decoded.role != 99) {
@@ -28,8 +36,18 @@ function CourseInd() {
                        <div className={`${buttonOpen ? "ms-[221px]" : "ms-[55.5px]"} flex-grow`}>
                                 <div className="bg-gray-100 h-screen pt-12">
                                     <div className={`${buttonOpen ? "px-[130px] py-4 w-full max-w-[1800px] mx-auto" : "px-[200px] py-4 w-full max-w-[1800px] mx-auto"}`}>
-                                            <div className="text-gray-500">Dashboard / Course</div>
-                                            <div></div>
+                                            <div className="text-gray-500">Course / Module - {url.course_id}</div>
+                                            <div className="mt-5 font-semibold text-xl text-gray-600">Associated Modules</div>
+                                            <div className="mt-5 bg-white rounded px-8 py-10">
+                                                        <div className="font-semibold">
+                                                                <IconButton size="md" color="success" className="bg-green-200">
+                                                                            <Plus className="h-6 w-6" />
+                                                                </IconButton>
+                                                        </div>
+                                                        <div className="mt-10">
+                                                            <EnhancedTable />
+                                                        </div>
+                                            </div>
                                     </div>
                                 </div>
                         </div>
