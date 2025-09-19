295397145cd01308696d499d8eb4120c6a6cca76
Prasath Narayanan <prasathnarayanan6@gmail.com>
2025-09-19 12:46:42 +0530

    pushing and changes in the Chpater API

--- client/src/API/getChapterAPI.js
+++ client/src/API/getChapterAPI.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+import APP_URL from "./config";
 
 function getChapterAPI(token, course_id) {
-    const result = axios.get(`http://localhost:4004/api/v1/get-chapter?course_id=${course_id}`, {
+    const result = axios.get(APP_URL+`/api/v1/get-chapter?course_id=${course_id}`, {
         headers: {
             Authorization: `Bearer ${token}`,
         },
