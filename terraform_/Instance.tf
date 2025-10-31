4d20ec8488bf0b814b004316efc40f3f2db1f892
Prasath Narayanan <prasathnarayanan6@gmail.com>
2025-10-31 21:30:09 +0530

    Changes

---
+++ terraform_/Instance.tf
@@ -0,0 +1,8 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+  owners = ["099720109477"]
+}
