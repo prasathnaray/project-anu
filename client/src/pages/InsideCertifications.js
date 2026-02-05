299ebfc383db765e750766450f3cbe7e5481bc8d
Prasath Narayanan <prasathnarayanan6@gmail.com>
2026-02-05 17:56:30 +0530

    fix: Remove unused close button from completion popup for cleaner UI

--- client/src/pages/InsideCertifications.js
+++ client/src/pages/InsideCertifications.js
@@ -1033,7 +1033,7 @@ function InsideCertifications() {
               </div>
               <button
                 onClick={closeCompletionPopup}
-                className="text-gray-400 hover:text-gray-600 transition-colors ml-4"
+                className="text-red-500 hover:text-red-500 transition-colors ml-4"
               >
                 <X size={24} />
               </button>
@@ -1101,14 +1101,6 @@ function InsideCertifications() {
             </div>
 
             {/* Footer */}
-            <div className="flex justify-end gap-3 pt-4 border-t">
-              <button
-                onClick={closeCompletionPopup}
-                className="px-6 py-2 bg-gray-200 text-gray-700 rounded-lg hover:bg-gray-300 transition-colors"
-              >
-                Close
-              </button>
-            </div>
           </div>
         </div>
       )}
