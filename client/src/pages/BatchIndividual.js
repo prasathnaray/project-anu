0a58ed926c40bb57061ed6edc2c4fd2b67fdf2f8
Prasath Narayanan <prasathnarayanan6@gmail.com>
2026-02-05 11:09:19 +0530

    fix: Remove commented-out code for Instructor vs Trainee Ratio section in BatchIndividual

--- client/src/pages/BatchIndividual.js
+++ client/src/pages/BatchIndividual.js
@@ -99,11 +99,6 @@ function BatchIndividual() {
                                                 {jwtDecode(localStorage.getItem('user_token')).role == 101 && (
                                                         <div className="col-span-1 bg-white">
                                                                 <div className="p-2">Instructor vs Trainee Ratio</div>
-                                                                {/* {jwtDecode(localStorage.getItem('user_token')).role == 101 && (
-                                                                        <div>
-                                                                                <TraineeInsRatio trainee={traineeCount} instructor={instructorCount}/>     
-                                                                        </div>
-                                                                )} */}
                                                                 <div>
                                                                         <TraineeInsRatio trainee={traineeCount} instructor={instructorCount}/>     
                                                                 </div>
