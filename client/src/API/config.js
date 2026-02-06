4a3a3b377897a1d57b824beb155f48c1a3a2fcd8
Prasath Narayanan <prasathnarayanan6@gmail.com>
2026-02-06 12:22:35 +0530

    Add new files

--- client/src/API/config.js
+++ client/src/API/config.js
@@ -1,3 +1,3 @@
-const APP_URL = 'https://1191-103-158-43-37.ngrok-free.app';
-//const APP_URL = 'http://localhost:4004'
+// const APP_URL = 'https://1191-103-158-43-37.ngrok-free.app';
+const APP_URL = 'http://localhost:4004'
 export default APP_URL;
