6625189f8025209a941c2a362c3234002fc73929
Prasath Narayanan <prasathnarayanan6@gmail.com>
2026-02-13 16:45:21 +0530

    Refactor code structure for improved readability and maintainability

--- client/src/API/config.js
+++ client/src/API/config.js
@@ -1,4 +1,4 @@
 // const APP_URL = 'https://1191-103-158-43-37.ngrok-free.app';
-//const APP_URL = 'http://13.205.207.179:4004'
-const APP_URL = 'http://10.42.249.88:4004';
+const APP_URL = 'http://13.205.207.179:4004'
+// const APP_URL = 'http://10.42.249.88:4004';
 export default APP_URL;
